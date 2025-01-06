f64b0762620919959cbc1370cb6a393d469d77cf
Naman Verama <149177973+Namanv0509@users.noreply.github.com>
2025-01-06 14:56:37 +0530

    Update 01. Find the nth fibonacci number.cpp

--- 14. Dynamic Programming/1. Intro to DP/01. Find the nth fibonacci number.cpp
+++ 14. Dynamic Programming/1. Intro to DP/01. Find the nth fibonacci number.cpp
@@ -18,29 +18,15 @@ Complexity Analysis:
 */
 
 // Memoization
-int fibo(int n, vector<int>& dp) {
-    if (n == 0 || n == 1)
-        return dp[n] = n;
-    if (dp[n] != -1)
-        return dp[n];
-    return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
-}
+class Solution:
+    def fib(self, n: int, memo = {}) -> int:
+        if (n in memo):
+            return memo[n]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
 
-// Tabulation
-int fiboTab(int n){
-    vector<int> dp(n+1);
-    dp[0] = 0, dp[1] = 1;
-    for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
-    }
-    return dp[n];
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> dp(n + 1, -1);
-    fibo(n, dp);
-    cout << dp[n];
-    return 0;
-}
+        memo[n] = self.fib(n-1, memo) + self.fib(n-2, memo)
+        return memo[n]
+        
