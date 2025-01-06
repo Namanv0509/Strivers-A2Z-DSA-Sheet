db2c4b58cfcbd0f78f252ada2113beb3dcf958a2
Naman Verama <149177973+Namanv0509@users.noreply.github.com>
2025-01-06 14:57:45 +0530

    Update 01. Climbing Stairs.cpp

--- 14. Dynamic Programming/2. 1D DP/01. Climbing Stairs.cpp
+++ 14. Dynamic Programming/2. 1D DP/01. Climbing Stairs.cpp
@@ -19,36 +19,14 @@ Complexity Analysis:
 
 
 // Memoization
-int fmemo(int n, vector<int>& dp) {
-    if (n == 1 || n == 2)
-        return dp[n] = n;
-    if (dp[n] != -1)
-        return dp[n];
-    return dp[n] = fmemo(n - 1, dp) + fmemo(n - 2, dp);
-}
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
 
-// Tabulation
-int ftab(int n){
-    vector<int> dp(n+1);
-    dp[1] = 1, dp[2] = 2;
-    for(int i=3; i<=n; i++)
-        dp[i] = dp[i-1]+dp[i-2];
-    return dp[n];
-}
-
-// Space Optimization
-int fopt(int n){
-    int p1 = 1, p2 = 2, ans = 0;
-    for(int i=3; i<=n; i++){
-        ans = p1+p2;
-        p1 = p2, p2 = ans;
-    }
-    return ans;
-}
-
-int climbStairs(int n) {
-    if (n == 1 || n == 2)
-        return n;
-    return fopt(n);
-}
-
+        dp = [0] * (n+1)
+        dp[0] = dp[1] = 1
+        
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
